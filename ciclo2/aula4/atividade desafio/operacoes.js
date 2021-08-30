7893d4e956cb5db9d4648941b007239399d23dcb
marinafiori <88670658+marinafiori@users.noreply.github.com>
2021-08-30 13:58:03 -0300

    Add files via upload

--- ciclo2/aula4/atividade desafio/operacoes.js
+++ ciclo2/aula4/atividade desafio/operacoes.js
@@ -2,54 +2,62 @@ window.onload=function(){
 
 //produtos
 (()=>{
+    let mostrarProdutosCliente = document.querySelector("#content-produtos > ul#produtos");
 
-	let mostrarProdutosCliente = document.querySelector("#content-produtos > ul#produtos");
-
-	for(let idx in produtos){
-		mostrarProdutosCliente.innerHTML += `<li class='itemProduto' data-preco=${produtos[idx].prodPreco}>${produtos[idx].prodDesc} </li>`
-	}
-
-})(produtos)
+    for(let idx in produtos){
+        mostrarProdutosCliente.innerHTML += `<li class="itemProduto" data-preco=${produtos[idx].prodPreco}>${produtos[idx].prodDesc}`        
+    }
+   })(produtos)
 
 //compra
-const itemProduto = document.querySelectorAll("#produtos > li.itemProduto");
-
+const itemProduto = document.querySelectorAll("ul#produtos > li.itemProduto");
 const cestaDoCliente = document.querySelector("ul#cestaDoCliente");
-
 const mostraTotalCompra = document.querySelector("#mostraTotalCompra");
-
-const armazenaIten=[];
-var totalPedido=0;
+const armazenaItem=[]; 
+var totalPedido =0;
 
 itemProduto.forEach((item)=>{
+    item.addEventListener("click",()=>{
+        
+        li = document.createElement("li");
+        li.setAttribute("data-preco", item.dataset.preco);
+        console.log(li);
+                if(armazenaItem.indexOf(item.textContent) == -1){
+            
+                armazenaItem.push(item.textContent); 
+           
+                cestaDoCliente.appendChild(li).textContent = item.textContent = item.textContent;
 
-	item.addEventListener('click',()=>{
-		//alert("lista");
-		li = document.createElement("li");
+            totalPedido += Number(item.dataset.preco); 
 
-		if(armazenaIten.indexOf(item.textContent) == -1){
+            
+            mostraTotalCompra.value = totalPedido.toLocaleString("pt-BR",
+            {style:"currency", currency: "BRL"})
+        }else{
+            alert(`Este item ${item.textContent} já está na sua cesta!`);
+        }
+    });
+});
 
-			armazenaIten.push(item.textContent);
+const cestaCliente = document.querySelectorAll("#cestaDoCliente"); 
 
-			cestaDoCliente.appendChild(li).textContent = item.textContent;	
-
-			totalPedido += Number(item.dataset.preco);
-
-			mostraTotalCompra.value = totalPedido.toLocaleString('pt-BR', 
-				{style: 'currency', currency:'BRL'}
-				)
-			
-
-		}else{
-			alert(`este item ${item.textContent} já está na sua cesta`)
-		}
-
-	})
-
+cestaCliente.forEach((item) => {
+    item.addEventListener("click", (itemCesta) => {
+        var idx = armazenaItem.indexOf(itemCesta.target.textContent);
+        if(idx > -1) {
+            totalPedido -= Number(itemCesta.target.dataset.preco);
+            cestaDoCliente.removeChild(cestaDoCliente.childNodes[idx]);
+            armazenaItem.splice(idx, 1);
+            mostraTotalCompra.value = totalPedido.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    
+        }    
+    })
 })
 
+}
 
 
-}//fim
+
+
 
 
